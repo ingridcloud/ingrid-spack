2f8a93d05049b52e62e8eb0137beb7ed0cb59d6b
Niclas Jansson <njansson@kth.se>
2020-04-24 21:19:45 +0200

    Fixed missing deps

--- packages/dolfin/package.py
+++ packages/dolfin/package.py
@@ -21,10 +21,12 @@ class Dolfin(AutotoolsPackage):
     variant('petsc', default=True)
     variant('parmetis', default=True)
     variant('mpi', default=True)
+    variant('xml', default=True)
 
     # FIXME: Add dependencies if required.
     depends_on('ufc2-hpc@2.1.4', when='@0.9.0p1-hpc:')
     #depends_on('ufc2-hpc@2.2.0', when='@0.9.1-hpc:')
+    depends_on('libxml2', when='@0.9.0p1-hpc+xml')
     depends_on('mpi', when='+mpi')
     depends_on('petsc', when='+petsc')
     depends_on('parmetis', when='+parmetis')
@@ -33,13 +35,16 @@ class Dolfin(AutotoolsPackage):
     def configure_args(self):
         # FIXME: Add arguments other than --prefix
         # FIXME: If not needed delete this function
-        args = ['']
+        args = []
         if '+mpi' in self.spec:
-            args.append('--enable-mpi --enable-mpi-io')
+            args.append('--enable-mpi')
+            args.append('--enable-mpi-io')
         if '+parmetis' in self.spec:
             args.append('--with-parmetis')
         if '+petsc' in self.spec:
             args.append('--with-petsc')
         if '+gts' in self.spec:
             args.append('--with-gts')
+        if '~xml' in self.spec:
+            args.append('--without-xml')
         return args
