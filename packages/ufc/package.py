5c9c87fe8c17f86ca44ea3246c281c94b271c561
Niclas Jansson <njansson@kth.se>
2020-07-20 22:09:06 +0200

    Ensure that PYTHONPATH's are set in modules

--- packages/ufc/package.py
+++ packages/ufc/package.py
@@ -6,22 +6,17 @@
 from spack import *
 
 
-class Ufc(CMakePackage):
+class Ufc(CMakePackage, PythonPackage):
     """UFC (Unified Form-assembly Code) is a unified framework for finite
        element assembly. More precisely, it defines a fixed interface for
        communicating low level routines (functions) for evaluating and
        assembling finite element variational forms."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://bitbucket.org/adaptivesimulations/ufc2-hpc"
     git      = "https://bitbucket.org/adaptivesimulations/ufc2-hpc.git"
 
     version('2.2.0', tag='v2.2.0')
     version('2.1.4', tag='v2.1.4')
 
-    def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
-        return args
+    depends_on('python@3:', type='run', when='2.2.0')
+    depends_on('python@:2', type='run', when='2.1.4')
